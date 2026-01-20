9e0a1b0bfa306a37c7d57442559c31f20c1c4418
Goldlabel Apps Ltd <goldlabel.apps@gmail.com>
2026-01-20 10:18:41 +0000

    Update serverUseConfig.ts

--- app/NX/lib/serverHooks/serverUseConfig.ts
+++ app/NX/lib/serverHooks/serverUseConfig.ts
@@ -1,7 +1,11 @@
 import { T_Config } from '../../types';
+import path from 'path';
+import fs from 'fs';
 
 export async function serverUseConfig(): Promise<T_Config> {
-    return {
-
-    };
+    const project = process.env.NEXT_PUBLIC_PROJECT || 'nx';
+    const configPath = path.join(process.cwd(), 'public', project, 'config.json');
+    const configRaw = fs.readFileSync(configPath, 'utf-8');
+    const config: T_Config = JSON.parse(configRaw);
+    return config;
 }
