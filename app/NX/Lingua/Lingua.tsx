b06d93cbf40d4497ad652fc20f5382cbcdd3eee8
Goldlabel Apps Ltd <goldlabel.apps@gmail.com>
2026-02-01 22:19:06 +0000

    Lingua

---
+++ app/NX/Lingua/Lingua.tsx
@@ -0,0 +1,14 @@
+// Lingua.tsx
+// Cartridge for localisation and more, based on Flash template
+
+import React from 'react';
+
+export const Lingua = () => {
+    return (
+        <div>
+            <h1>Lingua Cartridge</h1>
+            <p>This cartridge will handle localisation and more.</p>
+        </div>
+    );
+};
+
