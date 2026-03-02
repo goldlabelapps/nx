6ca2b5c296ec4daa73f3d2dc3f845d278f44672e
Goldlabel Apps Ltd <goldlabel.apps@gmail.com>
2026-03-02 05:51:03 +0000

    remove aki

--- public/echopay/flash/OldCompany/OldCompany.tsx
+++ public/echopay/flash/OldCompany/OldCompany.tsx
@@ -7,6 +7,11 @@ import {
 
 export default function OldCompany({ options }: I_OldCompany) {
 
+    React.useEffect(() => {
+        ActionScript.current = new NewCompanyAS(clipRef);
+        ActionScript.current.init();
+    }, [thisStep]);
+
     return (
         <Box>
             Load old Old Company by slug {options?.slug}
