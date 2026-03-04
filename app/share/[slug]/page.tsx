8c6607f0284aef9d552029136c1fb777414e30c4
Goldlabel Apps Ltd <goldlabel.apps@gmail.com>
2026-03-04 06:34:19 +0000

    Share page with slug

--- app/share/[slug]/page.tsx
+++ app/share/[slug]/page.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 
-interface SharePageProps {
-    params: { slug: string };
-}
 
 // This is a server component by default
-export default async function SharePage({ params }: SharePageProps) {
-    const { slug } = await params;
+export default async function SharePage(props: PageProps<'/share/[slug]'>) {
+    const params = await props.params;
+    const { slug } = params;
     // You can fetch data here if needed (e.g., await fetch(...))
     return (
         <div>
             <h1>Share</h1>
             <p>slug: {slug}</p>
-            <pre>{JSON.stringify(await params, null, 2)}</pre>
+            <pre>{JSON.stringify(params, null, 2)}</pre>
         </div>
     );
 }
