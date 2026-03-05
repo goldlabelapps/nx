6d0b80da386a6c8bf1e64a09009c3947b8e61ebb
Goldlabel Apps Ltd <goldlabel.apps@gmail.com>
2026-03-05 06:04:43 +0000

    mx-admin

--- app/share/[slug]/page.tsx
+++ app/share/[slug]/page.tsx
@@ -182,13 +182,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
                     <CardActions>
                         <Virus meta={mergedMeta} />
                         <Box sx={{ flexGrow: 1 }} />
-                        <Button
-                            variant="contained"
-                            color="primary"
 
-                        >
-                            New Share
-                        </Button>
                     </CardActions>
                 </Card>
 
